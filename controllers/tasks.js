6d1b184f056ee9f9bb31604868e180ffdd222cd1
bnewvillage <blainenewvillage@gmail.com>
2025-04-16 23:37:16 +0400

    fix gettask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -10,7 +10,6 @@ const getTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({ message: 'Task not found' });
         }
-
         res.status(200).json(task);
     } catch (err) {
         res.status(500).json({ message: err.message || 'Internal Server Error.' });
